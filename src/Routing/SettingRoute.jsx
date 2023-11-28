a73b1639526d0a013a8904f18f949b17eedaac43
JuyeongWoo <dlsrtyex63@gmail.com>
2023-11-28 10:17:56 +0900

    fix: 병합 해결

--- src/Routing/SettingRoute.jsx
+++ src/Routing/SettingRoute.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Mypage from "../pages/Mypage/Mypage";
 import MyFamily from "../pages/MyFamily/MyFamily";
+import Mypage from "../pages/Mypage/Mypage";
 
 function SettingRoute() {
     return (
         <Routes>
             <Route path="myfamily" element={<MyFamily />} />
             <Route path="mypage" element={<Mypage />} />
-            <Route path="myfamily" element={<></>} />
         </Routes>
     );
 }
