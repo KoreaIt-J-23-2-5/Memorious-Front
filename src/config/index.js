062b127236e84501b9d262b3f3723c476ee5f012
SeongGwangJu <seonggwangdev@gmail.com>
2024-01-05 06:24:42 +0900

    feat: 탄력적ip로 변경
    for deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://43.201.89.47",
     headers: {
         Authorization: localStorage?.getItem("accessToken"),
     },
